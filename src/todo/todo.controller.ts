31041f6b381d1e150072b8bbe59ed7c23ea135ef
takahiro <takapi854@gmail.com>
2023-06-23 00:03:31 +0900

    複数件のuserを種痘(降順)

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -15,58 +15,55 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-@Controller('todo')
+@Controller('user')
 export class TodoController {
   @Get('list')
   async listTasks() {
-    const result = await prisma.task.findMany({
-      where: {
-        is_done: false,
-      },
+    const result = await prisma.user.findMany({
       orderBy: {
-        id: 'asc',
+        id: 'desc',
       },
     });
     return [...result];
   }
 
-  @Post('')
-  async postTask(@Body() task: CreateTaskDto) {
-    const result = await prisma.task.create({
-      data: task,
-    });
-    return {
-      status: '201',
-    };
-  }
+  // @Post('')
+  // async postTask(@Body() task: CreateTaskDto) {
+  //   const result = await prisma.task.create({
+  //     data: task,
+  //   });
+  //   return {
+  //     status: '201',
+  //   };
+  // }
 
-  @Put(':id/done')
-  async updateTasks(@Param() param: UpdateTaskDto) {
-    await prisma.task.updateMany({
-      data: {
-        is_done: true,
-      },
-      where: {
-        id: parseInt(param.id),
-      },
-    });
-    return {
-      status: '204',
-    };
-  }
+  // @Put(':id/done')
+  // async updateTasks(@Param() param: UpdateTaskDto) {
+  //   await prisma.task.updateMany({
+  //     data: {
+  //       is_done: true,
+  //     },
+  //     where: {
+  //       id: parseInt(param.id),
+  //     },
+  //   });
+  //   return {
+  //     status: '204',
+  //   };
+  // }
 
-  @Delete(':id')
-  async deleteTask(@Param() param: DeleteTaskDto) {
-    await prisma.task.delete({
-      // data: {
-      //   is_done: true,
-      // },
-      where: {
-        id: parseInt(param.id),
-      },
-    });
-    return {
-      status: '204',
-    };
-  }
+  // @Delete(':id')
+  // async deleteTask(@Param() param: DeleteTaskDto) {
+  //   await prisma.task.delete({
+  //     // data: {
+  //     //   is_done: true,
+  //     // },
+  //     where: {
+  //       id: parseInt(param.id),
+  //     },
+  //   });
+  //   return {
+  //     status: '204',
+  //   };
+  // }
 }
